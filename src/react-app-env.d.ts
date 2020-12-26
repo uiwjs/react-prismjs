2880220b9f6bc536bb3ae5f1a3b0c3ddb527cf36
jaywcjlove <398188662@qq.com>
2020-12-26 23:34:22 +0800

    chore(deps): update dependency kkt to v6.0.11

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,8 +1,1 @@
-/// <reference types="node" />
-/// <reference types="react" />
-/// <reference types="react-dom" />
-
-declare module '*.less' {
-  const src: string;
-  export default src;
-}
+/// <reference types="react-scripts" />
